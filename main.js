5582425357fc0416e06e1a61acdf78fe3ddd095b
Simon Burg <semburg.sb@gmail.com>
2021-09-13 11:18:26 +0200

    done

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const range = document.getElementById('range')
-const numbr = document.getElementById('number-visible')        
+const numbr = document.getElementById('number-visible')
 
 range.addEventListener('input', (e) => {
-    const value = e.target.value
+    const value = +e.target.value
 
     const range_width = getComputedStyle(e.target).getPropertyValue('width')
 
     const label_width = getComputedStyle(numbr).getPropertyValue('width')
-    console.log( range_width, )
 
+    const num_width = +range_width.substring(0, range_width.length - 2)
+    const num_label_width = +label_width.substring(0, label_width.length - 2)
+
+    // MOVING the result window with changes:
+    const max = +e.target.max
+    const min = +e.target.min
+
+    const left = value * (num_width / max) - num_label_width / 2
+
+
+    numbr.style.left = `${left}px`
 
     numbr.innerHTML = value
 
